82dfdd875210dc9b928fe0e1bdbdd8edbeb2caac
Vainius <vmykolaitis1@gmail.com>
2023-08-07 13:02:08 +0300

    Remove some of the stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://vaynemyko.com',
+  site: 'https://rhyzetoken.com/',
   integrations: [
     tailwind(),
     svelte(),
